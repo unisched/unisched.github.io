29f2ad3260825ca541a15798431ccfb87a7c6489
Yurii Kadirov <admin@sirkadirov.com>
2024-03-16 16:53:43 +0200

    New version of the website

--- assets/js/site.js
+++ assets/js/site.js
@@ -16,4 +16,27 @@ document.addEventListener('DOMContentLoaded', () => {
             closeModal($target);
         });
     });
+
+    function initiateAppDownload () {
+        const userAgent = navigator.userAgent.toLowerCase();
+
+        // On Android-powered devices, redirect to the Play Store
+        if (userAgent.indexOf("android") > -1) {
+            window.location = 'https://play.google.com/store/apps/details?id=download.unisched.app';
+            return;
+        }
+
+        // On Windows-powered devices, redirect to the Microsoft Store
+        if (userAgent.indexOf("windows") > -1) {
+            window.location = 'https://apps.microsoft.com/detail/UniSched/9pg4lx8swxcm?launch=true&mode=mini';
+            return;
+        }
+
+        // On other devices, redirect to the web app
+        window.location = 'https://app.unisched.download/';
+    }
+
+    (document.querySelectorAll('.download-button') || []).forEach(($button) => {
+        $button.addEventListener('click', initiateAppDownload);
+    });
 });
